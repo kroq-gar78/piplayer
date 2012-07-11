7fb7a9714691ab49d005d9b38ca295d8f4fc754a
Aditya Vaidya <kroq.gar78@gmail.com>
2012-07-11 02:57:34 -0500

    run.sh: exit on error, compile only if PiPlayer.class doesn't exist

--- run.sh
+++ run.sh
@@ -1,9 +1,13 @@
-#!/bin/sh
+#!/bin/sh -eu
 
 # Runs PiPlayer by Aditya Vaidya <kroq.gar78@gmail.com>
 # Watch out! It can generate a LOT of output (1 million digits of Pi)
 # if left unchecked!
 
-javac PiPlayer.java && (java PiPlayer)
+if [ ! -f ./PiPlayer.class ] ; then
+	javac PiPlayer.java
+fi
+
+java PiPlayer
 
 echo # add blank line at the end
