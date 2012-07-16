5ab022de1500ee3e7fd0eb6d9da708f51cd98e47
Aditya Vaidya <kroq.gar78@gmail.com>
2012-07-15 19:16:02 -0500

    Added COPYING, which is the GPL v3, and added license header to PiPlayer.java

--- PiPlayer.java
+++ PiPlayer.java
@@ -1,6 +1,6 @@
 /* 
  * PiPlayer.java
- * Created by Aditya Vaidya <kroq.gar78@gmail.com>
+ * Copyright 2012 Aditya Vaidya <kroq.gar78@gmail.com>
  * 
  * Plays the first 1 million digits of Pi based on a certain key (what 
  * that key is, is yet to be determined :P)
@@ -9,6 +9,22 @@
  * Digits of Tau obtained from http://tauday.com/tau-digits.
  * 
  * All note frequencies taken from http://en.wikipedia.org/wiki/Piano_key_frequencies
+ * 
+ * 
+ * This file is part of PiPlayer.
+ * 
+ * PiPlayer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * PiPlayer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
  * 
  */
 
